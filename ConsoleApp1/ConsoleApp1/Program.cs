05f950e08605dbb9b8f3e8e82ba2c886f760331f
vxisxk <vaisak@protonmail.com>
2023-02-13 14:57:28 +0530

    first commit

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Enter first number");
+int n1 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Enter second number");
+int n2 = int.Parse(Console.ReadLine());
+
+int temp = n2;
+n2 = n1;
+n1 = temp;
+
+Console.WriteLine($"The swapped numbers are {n1} and {n2}");
