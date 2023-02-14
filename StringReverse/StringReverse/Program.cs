0c2d424731b2f349c6959662e088464eeb1102c2
vxisxk <vaisak@protonmail.com>
2023-02-14 16:22:28 +0530

    first commit

---
+++ StringReverse/StringReverse/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Write("Enter a String : ");
+string name = Console.ReadLine();
+string reverseString = string.Empty;
+for (int i = name.Length - 1; i >= 0; i--)
+{
+    reverseString += name[i];
+}
+Console.Write($"Reverse String is : {reverseString} ");
